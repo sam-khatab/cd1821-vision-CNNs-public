2e44d2c1b1ce435583ac00c7de092c89cea2b3bf
Shaady Khatab <shaady.khatab@cariad.technology>
2026-05-27 08:17:57 +0200

    Added dropout as parameter to get_model_transfer_learning

--- project/src/transfer.py
+++ project/src/transfer.py
@@ -4,7 +4,7 @@ import torchvision.models as models
 import torch.nn as nn
 
 
-def get_model_transfer_learning(model_name="resnet18", n_classes=50):
+def get_model_transfer_learning(model_name="resnet18", n_classes=50, dropout=0.3) -> torch.nn.Module:
 
     # Get the requested architecture
     if hasattr(models, model_name):
@@ -33,7 +33,7 @@ def get_model_transfer_learning(model_name="resnet18", n_classes=50):
     # 2. Create a new linear layer with the appropriate number of inputs and
     #    outputs
     model_transfer.fc1 = nn.Linear(num_ftrs, 64)
-    model_transfer.Dropout = nn.Dropout(p=0.3)
+    model_transfer.Dropout = nn.Dropout(p=dropout)
     model_transfer.fc2 = nn.Linear(64, n_classes)
 
     return model_transfer
